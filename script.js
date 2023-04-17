67881b37a0a5095f6796480be7d1a2fa7f1a6219
Shohrux <topilovshohrux696@gmail.com>
2023-04-17 12:58:13 +0300

    version_1

---
+++ script.js
@@ -0,0 +1,9 @@
+// for (let i = 1; i <=10; i++){
+//     console.log(`2 * ${i} = ${2*i}`);
+// }
+
+for (let i = 1; i <=10; i++){
+    for (let j = 1; j <= 10; j++){
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+}
